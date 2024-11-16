5743499f8a01f3c43b23f5f99219c28fd60839d4
Justin Bell <jmb0288@auburn.edu>
2024-11-16 09:42:05 -0600

    Initial Commit

---
+++ MyAdventure/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyAdventure
+//
+//  Created by Justin Bell on 11/16/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
